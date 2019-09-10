2b842b73398d9bc0c0a676a78449e674adf1358d
incredilejagur <danielminter13@gmail.com>
2019-09-10 21:57:09 +0200

    added check column function

--- suduko.js
+++ suduko.js
@@ -1,46 +1,48 @@
 function sudoku() {
 
     let array = [
-        [0, 1, 0, 4, 0, 5, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0,  0, 0, 0,  0, 0, 0],
+        [0, 0, 0,  0, 0, 0,  0, 0, 0],
+        [0, 0, 0,  0, 0, 0,  0, 0, 0],
 
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0,  0, 0, 0,  0, 0, 0],
+        [0, 0, 0,  0, 0, 0,  0, 0, 0],
+        [0, 0, 0,  0, 0, 0,  0, 0, 0],
 
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        [0, 0, 0,  0, 0, 0,  0, 0, 0],
+        [0, 0, 0,  0, 0, 0,  0, 0, 0],
+        [0, 0, 0,  0, 0, 0,  0, 0, 0]
     ]
 
     let num = 0;
 
     function generateNumber(num) {
-        let random_Number = Math.floor(Math.random() * (num)) + 1;
+        let random_Number = Math.floor(Math.random() * (num)) + 0;
         return random_Number
     }
 
     function intialBoard(level) {
         let a = 0;
         let b = 0;
-        if (level === "easy") level = 35
-        if (level === "intermediate") level = 25
-        if (level === "hard") level = 18
-        if (level === "expert") level = 10
-        // console.log(array[2])
-        for (var z = 0; z < level; z++) {
+        let rounds = 0;
+        if (level === "easy") rounds = 35
+        if (level === "intermediate") rounds = 25
+        if (level === "hard") rounds = 18
+        if (level === "expert") rounds = 10
+
+        for (var z = 0; z < rounds; z++) {
             let row = generateNumber(8)
             let col = generateNumber(8)
-            let random = generateNumber()
-            if (checkRow(row) === true ) {  
-                // checkColumn(col) === true
+            let random = generateNumber(8)
+            if (checkRow(row) === true && checkColumn(col) === true) {  
+                
                 a+= 1
                 array[row][col] = num || random
-            }else{b += 1
-                // return;
-            }
+            }else{b += 1}
         }
+
+        console.log("________________________________________________")
+        console.log(`**${level}**`)
         console.log("success:"+a, "|duplicates:" +b, "|total:"+ (a+b))
         TESTING()
     }
@@ -61,29 +63,19 @@ function sudoku() {
     return true;
     }
 
-    // function checkColumn(col){
-    //     // console.log(col + "  column")
-    //     for(var k  = 0; k < array.length;k++){
-    //         let columns = array[k][col]
-    //         if(columns === col){
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-    // }
-
-//     else{
-//         console.log("row:"+row,"col:"+col,random)
-//         console.log('duplicate found')
-// }
+    function checkColumn(col){
+        for(var k  = 0; k < array.length;k++){
+            let columns = array[k][col]
+            if(columns === num){
+             console.log('loc---> row/col:'+ k+"/"+col + ' number:'+num)
+                return false;
+            }
+        }
+        return true;
+    }
 
 
-// for(var j = 0; z < number.length;j++){
-//     let index = number.indexOf(random)
-//     if(index > -1){
-//         number.splice(index, 1)
-//     }else{console.log("error")}
-// }
+
 
 
 
--- test/suduko.test.js
+++ test/suduko.test.js
@@ -1,26 +1,23 @@
-describe('Suduku ', function () {
 
   describe('Testing different levels', function () {
-  it('Should give me 12 numbers as the level is easy. For testing the number 0 is blank.', function () {
+  it('Should give me 35 numbers as the level is easy. ', function () {
     let instance = sudoku()
    instance.intialBoard("easy")
    console.log("--------------------")
   });
 
-  it('Should give me 2 numbers as level is intermediate. For testing the number 0 is blank.', function () {
+  it('Should give me 25 numbers as level is intermediate. ', function () {
+    let instance = sudoku()
+    instance.intialBoard("intermediate")
+  });
+
+  it('Should give me 18 numbers as level is hard. ', function () {
+    let instance = sudoku()
+    instance.intialBoard("hard")
+  });
+
+  it('Should give me 10 numbers as level is expert. ', function () {
     let instance = sudoku()
     instance.intialBoard("expert")
   });
-
-  // it('Should give me 2 numbers as level is hard. For testing the number 0 is blank.', function () {
-  //   let instance = sudoku()
-  //   instance.intialBoard("hard")
-  // });
-
-  // it('Should give me 2 numbers as level is expert. For testing the number 0 is blank.', function () {
-  //   let instance = sudoku()
-  //   instance.intialBoard("expert")
-  // });
 })
-
-  });
