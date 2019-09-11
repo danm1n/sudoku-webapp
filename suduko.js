a690b91b99d4eca06aad85bac534b4215d27af77
Daniel <danielminter13@gmail.com>
2019-09-11 15:59:53 +0200

    different test method

--- suduko.js
+++ suduko.js
@@ -1,24 +1,25 @@
 function sudoku() {
 
     let array = [
-        [0, 0, 0,  0, 0, 0,  0, 0, 0],
-        [0, 0, 0,  0, 0, 0,  0, 0, 0],
-        [0, 0, 0,  0, 0, 0,  0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
 
-        [0, 0, 0,  0, 0, 0,  0, 0, 0],
-        [0, 0, 0,  0, 0, 0,  0, 0, 0],
-        [0, 0, 0,  0, 0, 0,  0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
 
-        [0, 0, 0,  0, 0, 0,  0, 0, 0],
-        [0, 0, 0,  0, 0, 0,  0, 0, 0],
-        [0, 0, 0,  0, 0, 0,  0, 0, 0]
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
     ]
 
     let num = 0;
 
-    function generateNumber(num) {
-        let random_Number = Math.floor(Math.random() * (num)) + 0;
-        return random_Number
+    function generateNumber(min,max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
     function intialBoard(level) {
@@ -32,42 +33,66 @@ function sudoku() {
         if (level === "expert") rounds = 10
 
         for (var z = 0; z < rounds; z++) {
-            let row = generateNumber(8)
-            let col = generateNumber(8)
-            let random = generateNumber(8)
-            if (checkRow(row) === true && checkColumn(col) === true) {  
-                
-                a+= 1
-                array[row][col] = num || random
-            }else{b += 1}
+            let row = generateNumber(0,8)
+            let col = generateNumber(0,8)
+            // console.log(row)
+            let random = generateNumber(1,9)
+            if (checkRow(row,random) === true) {
+                a += 1
+                array[row][col] = random
+            } else { b += 1 }
         }
 
-        console.log("success:"+a, "|duplicates:" +b, "|total:"+ (a+b))
+        console.log("success:" + a, "|duplicates:" + b, "|total:" + (a + b))
         TESTING()
-           console.log("---------END-----------")
+        console.log("---------END-----------")
     }
 
-    function checkRow(row) {
-        let array_x = array[row]      
-        let number = [1,2,3,4,5,6,7,8,9]
-        number = number.filter(function(val) {
+    function checkRow(row,random) {
+        // console.log(random)
+        let available = 0;
+        let array_x = array[row]
+        let number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        number = number.filter(function (val) {
             return array_x.indexOf(val) == -1;
-          });
-          num = number[generateNumber(number.length - 1)]
+        });
+        // console.log(number)
+        // console.log(`sorted:${number}||row:${row}`)
+        for(var k = 0; k <number.length;k++){
+            // console.log(k)
+            // console.log(`avail :${number[k] === random}, number:${random} array:${number} index:${k}`)
+            if(number[k] === random){
+                available = random;
+                break;
+            }else{
+               random = generateNumber(1,9)
+               k=0
+             }
+        }
+// console.log(add)
+        // num = number[generateNumber(number.length - 1)]
         for (var z = 0; z < array_x.length; z++) {
-            if (array_x[z] === num) {
+            // console.log("_________________________")
+            // console.log(array_x[3])
+            // console.log(available + " avail")
+            // console.log(array_x[z] === available)
+            // console.log("_________________________")
+            console.log(`value:${array_x[z]}, number:${available}`)
+            if (array_x[z] === available){
                 console.log('duplicate')
                 return false;
+            }
         }
-    }
-    return true;
+        console.log("///////////////////////////////")
+        return true;
     }
 
-    function checkColumn(col){
-        for(var k  = 0; k < array.length;k++){
+
+    function checkColumn(col) {
+        for (var k = 0; k < array.length; k++) {
             let columns = array[k][col]
-            if(columns === num){
-             console.log('loc---> row/col:'+ k+"/"+col + ' number:'+num)
+            if (columns === num) {
+                console.log('loc---> row/col:' + k + "/" + col + ' number:' + num)
                 return false;
             }
         }
--- test/suduko.test.js
+++ test/suduko.test.js
@@ -5,18 +5,18 @@
    instance.intialBoard("easy")
   });
 
-  it('Should give me 25 numbers as level is intermediate. ', function () {
-    let instance = sudoku()
-    instance.intialBoard("intermediate")
-  });
+  // it('Should give me 25 numbers as level is intermediate. ', function () {
+  //   let instance = sudoku()
+  //   instance.intialBoard("intermediate")
+  // });
 
-  it('Should give me 18 numbers as level is hard. ', function () {
-    let instance = sudoku()
-    instance.intialBoard("hard")
-  });
+  // it('Should give me 18 numbers as level is hard. ', function () {
+  //   let instance = sudoku()
+  //   instance.intialBoard("hard")
+  // });
 
-  it('Should give me 10 numbers as level is expert. ', function () {
-    let instance = sudoku()
-    instance.intialBoard("expert")
-  });
+  // it('Should give me 10 numbers as level is expert. ', function () {
+  //   let instance = sudoku()
+  //   instance.intialBoard("expert")
+  // });
 })
