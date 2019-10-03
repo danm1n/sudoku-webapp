40139fe886f6be25674e7c02c4d7340ab4091776
Daniel <danielminter13@gmail.com>
2019-10-03 20:04:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import './styling/index.css';
 import App from './App.jsx';
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 render(
     
@@ -13,4 +13,4 @@ render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+serviceWorker.register();
