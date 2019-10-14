902d84dc79ba4c7586d1c8aae4d966f082461543
Daniel <danielminter13@gmail.com>
2019-10-14 23:12:17 +0200

    fixed timezone diff with logger

--- services/user/logger.js
+++ services/user/logger.js
@@ -1,6 +1,6 @@
 module.exports = (pool) => {
 let today = new Date();
-let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+let time = today.getHours()+3 + ":" + today.getMinutes() + ":" + today.getSeconds();
 let log_it = async (username,event) => {
     console.log('test')
 await pool.query(`insert into logger (username,event,time) values($1,$2,$3)`,[username,event,time])
