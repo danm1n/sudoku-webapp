03bcc8253f317e7e7bf9ee10f80d9adce1a0c79a
Daniel <danielminter13@gmail.com>
2019-10-14 23:16:46 +0200

    timezone fix on logger

--- services/user/logger.js
+++ services/user/logger.js
@@ -1,6 +1,6 @@
 module.exports = (pool) => {
 let today = new Date();
-let time = today.getHours()+3 + ":" + today.getMinutes() + ":" + today.getSeconds();
+let time = today.getHours()+2 + ":" + today.getMinutes() + ":" + today.getSeconds();
 let log_it = async (username,event) => {
     console.log('test')
 await pool.query(`insert into logger (username,event,time) values($1,$2,$3)`,[username,event,time])
